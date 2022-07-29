2111a3cbb2224e665aef00736b572b4f4e50942b
smartcoder0305 <stunningcoder7@gmail.com>
2022-07-29 15:14:23 +0200

    Fix aria-label when status is absent

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -57,7 +57,7 @@ export const Key = ({
   return (
     <button
       style={styles}
-      aria-label={`${value} ${status}`}
+      aria-label={`${value}${status ? ' ' + status : ''}`}
       className={classes}
       onClick={handleClick}
     >
