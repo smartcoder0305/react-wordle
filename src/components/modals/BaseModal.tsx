c7ebfb053c195e5a88d523af469dfc9b6b79b147
smartcoder0305 <stunningcoder7@gmail.com>
2022-08-25 22:02:51 +0200

    Remove modal scroll where not needed and modal centering

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -17,7 +17,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
         className="fixed inset-0 z-10 overflow-y-auto"
         onClose={handleClose}
       >
-        <div className="flex min-h-screen items-center justify-center py-10 px-4 text-center sm:block sm:p-0">
+        <div className="flex min-h-full items-center justify-center py-10 px-4 text-center sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -27,16 +27,8 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 min-h-screen bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
-
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="hidden sm:inline-block sm:h-screen sm:align-middle"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
