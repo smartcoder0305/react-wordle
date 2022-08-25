45d2b6962d9776c0fbb2fa2e335eaf62fae9b95f
smartcoder0305 <stunningcoder7@gmail.com>
2022-08-25 21:57:46 +0200

    Center grid vertically

--- src/App.tsx
+++ src/App.tsx
@@ -291,7 +291,7 @@ function App() {
         )}
 
         <div className="mx-auto flex w-full grow flex-col px-1 pt-2 pb-8 sm:px-6 md:max-w-7xl lg:px-8 short:pb-2 short:pt-2">
-          <div className="grow pb-6 short:pb-2">
+          <div className="flex grow flex-col justify-center pb-6 short:pb-2">
             <Grid
               solution={solution}
               guesses={guesses}
