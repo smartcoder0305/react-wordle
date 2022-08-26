addf6457942234ee840a453b60a2a0fc94f95fff
smartcoder0305 <stunningcoder7@gmail.com>
2022-08-27 01:16:55 +0200

    added isComplete to cells

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -21,37 +21,37 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
           value="W"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="E" isCompleted={true} />
+        <Cell value="A" isCompleted={true} />
+        <Cell value="R" isCompleted={true} />
+        <Cell value="Y" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter W is in the word and in the correct spot.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="P" isCompleted={true} />
+        <Cell value="I" isCompleted={true} />
         <Cell
           isRevealing={true}
           isCompleted={true}
           value="L"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="O" isCompleted={true} />
+        <Cell value="T" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter L is in the word but in the wrong spot.
       </p>
 
       <div className="mb-1 mt-4 flex justify-center">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
+        <Cell value="V" isCompleted={true} />
+        <Cell value="A" isCompleted={true} />
+        <Cell value="G" isCompleted={true} />
         <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="E" isCompleted={true} />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
